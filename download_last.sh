ad8c7fb56dfb0f6df434cac8de88a7789d6c156a
elperno <elperno@gmail.com>
2017-03-25 20:38:54 +0100

    Scripts for mp3 downloading

---
+++ download_last.sh
@@ -0,0 +1,2 @@
+sudo curl -L https://yt-dl.org/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
---
+++ mp3_from_youtube.sh
@@ -0,0 +1,1 @@
+/usr/local/bin/youtube-dl --extract-audio --audio-format mp3 $1 
